b875553fd60132c565cb39ce0e6fddf0138efd7a
Om Khadka <om.khadka.101@icloud.com>
2025-09-06 19:16:57 -0400

    init
    init push

---
+++ src/labs/scripted/agents/ClosestUnitAgent.java
@@ -0,0 +1,95 @@
+package src.labs.scripted.agents;
+
+
+// SYSTEM IMPORTS
+import edu.cwru.sepia.action.Action;                                        // how we tell sepia what each unit will do
+import edu.cwru.sepia.agent.Agent;                                          // base class for an Agent in sepia
+import edu.cwru.sepia.environment.model.history.History.HistoryView;        // history of the game so far
+import edu.cwru.sepia.environment.model.state.ResourceNode;                 // tree or gold
+import edu.cwru.sepia.environment.model.state.ResourceNode.ResourceView;    // the "state" of that resource
+import edu.cwru.sepia.environment.model.state.ResourceType;                 // what kind of resource units are carrying
+import edu.cwru.sepia.environment.model.state.State.StateView;              // current state of the game
+import edu.cwru.sepia.environment.model.state.Unit.UnitView;                // current state of a unit
+import edu.cwru.sepia.util.Direction;                                       // directions for moving in the map
+
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+// JAVA PROJECT IMPORTS
+
+
+
+public class ClosestUnitAgent
+    extends Agent
+{
+
+    // put your fields here! You will probably want to remember the following information:
+    //      - all friendly unit ids (there may be more than one!)
+    //      - the enemy unit id
+    //      - the id of the gold
+
+
+    /**
+     * The constructor for this type. The arguments (including the player number: id of the team we are controlling)
+     * are contained within the game's xml file that we are running. We can also add extra arguments to the game's xml
+     * config for this agent and those will be included in args.
+     */
+	public ClosestUnitAgent(int playerNum, String[] args)
+	{
+		super(playerNum); // make sure to call parent type (Agent)'s constructor!
+
+        // initialize your fields here!
+
+        // helpful printout just to help debug
+		System.out.println("Constructed ClosestUnitAgent");
+	}
+
+    /////////////////////////////// GETTERS AND SETTERS (this is Java after all) ///////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public Map<Integer, Action> initialStep(StateView state,
+                                            HistoryView history)
+	{
+        // TODO: identify units, set fields, and then decide what to do
+		return null;
+	}
+
+	@Override
+	public Map<Integer, Action> middleStep(StateView state,
+                                           HistoryView history)
+    {
+        Map<Integer, Action> actions = new HashMap<Integer, Action>();
+
+        // TODO: your code to give your unit actions for this turn goes here!
+
+        return actions;
+	}
+
+    @Override
+	public void terminalStep(StateView state,
+                             HistoryView history)
+    {
+        // don't need to do anything
+    }
+
+    /**
+     * The following two methods aren't really used by us much in this class. These methods are used to load/save
+     * the Agent (for instance if our Agent "learned" during the game we might want to save the model, etc.). Until the
+     * very end of this class we will ignore these two methods.
+     */
+    @Override
+	public void loadPlayerData(InputStream is) {}
+
+	@Override
+	public void savePlayerData(OutputStream os) {}
+
+}
+
