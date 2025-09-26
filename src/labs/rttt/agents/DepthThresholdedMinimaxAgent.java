0c4b58cf9cf8dec00e20683899dbfe369938d7a7
Om Khadka <om.khadka.101@gmail.com>
2025-09-26 14:22:25 -0400

    v1 of depth-limiting minimax func, implemented

--- src/labs/rttt/agents/DepthThresholdedMinimaxAgent.java
+++ src/labs/rttt/agents/DepthThresholdedMinimaxAgent.java
@@ -1,6 +1,5 @@
 package src.labs.rttt.agents;
 
-
 // SYSTEM IMPORTS
 import edu.bu.labs.rttt.agents.Agent;
 import edu.bu.labs.rttt.game.CellType;
@@ -14,7 +13,6 @@ import edu.bu.labs.rttt.utils.Pair;
 import java.util.List;
 import java.util.Map;
 
-
 // JAVA PROJECT IMPORTS
 import src.labs.rttt.heuristics.Heuristics;
 
@@ -22,20 +20,26 @@ import src.labs.rttt.heuristics.Heuristics;
 public class DepthThresholdedMinimaxAgent
     extends Agent
 {
-
+    //
+    // Fields
+    //
     public static final int DEFAULT_MAX_DEPTH = 3;
-
     private int maxDepth;
 
+    //
+    // Constructors
+    //
     public DepthThresholdedMinimaxAgent(PlayerType myPlayerType)
     {
         super(myPlayerType);
         this.maxDepth = DEFAULT_MAX_DEPTH;
     }
 
+    //
+    // Getter/Setter
+    //
     public final int getMaxDepth() { return this.maxDepth; }
     public void setMaxDepth(int i) { this.maxDepth = i; }
-
     public String getTabs(Node node)
     {
         StringBuilder b = new StringBuilder();
@@ -46,16 +50,75 @@ public class DepthThresholdedMinimaxAgent
         return b.toString();
     }
 
+    //
+    // Methods
+    //
+
+    // Runs the function to determine and play the best possible moves.
     public Node minimax(Node node)
     {
-        // uncomment if you want to see the tree being made
+        // // uncomment if you want to see the tree being made
         // System.out.println(this.getTabs(node) + "Node(currentPlayer=" + node.getCurrentPlayerType() +
         //      " isTerminal=" + node.isTerminal() + " lastMove=" + node.getLastMove() + ")");
 
-        /**
-         * TODO: complete me!
-         */
-        return null;
+        // If we @ terminal node, end
+        if (node.isTerminal()) {
+            node.setUtilityValue(node.getTerminalUtility());
+            return node;
+        }
+        
+        // if @ depth limit, ret the best node so far
+        if (node.getDepth() >= this.getMaxDepth()) {
+            double heuristicValue = Heuristics.calculateHeuristicValue(node);
+            node.setUtilityValue(heuristicValue);
+            return node;
+        }
+
+        // getting possible moves so far
+        List<Node> children = node.getChildren();
+        
+        // Checking if no moves avavil (SHOULDN'T HAPPEN, but sometimes does)
+        if (children.isEmpty()) {
+            double heuristicValue = Heuristics.calculateHeuristicValue(node);
+            node.setUtilityValue(heuristicValue);
+            return node;
+        }
+
+        Node bestChild = null;
+        double bestUtility;
+        
+        // init based on if its MAX/MIN turn
+        if (node.getCurrentPlayerType() == node.getMyPlayerType()) {
+            bestUtility = Double.NEGATIVE_INFINITY; // MAX
+        } else { 
+            bestUtility = Double.POSITIVE_INFINITY; // MIN
+        }
+
+        for (Node child : children) {
+            Node resultNode = minimax(child);
+            double childUtility = resultNode.getUtilityValue();
+            
+            // Update the best choice (dependant on playerType)
+            if (node.getCurrentPlayerType() == node.getMyPlayerType()) {
+                // MAX, find highest ulti
+                if (childUtility > bestUtility) {
+                    bestUtility = childUtility;
+                    bestChild = child;
+                }
+            } else {
+                // MIN, find the lowest util 
+                if (childUtility < bestUtility) {
+                    bestUtility = childUtility;
+                    bestChild = child;
+                }
+            }
+        }
+
+        // Set node's ulti to best one found
+        node.setUtilityValue(bestUtility);
+        
+        // ret the node with the best possible next move
+        return bestChild;
     }
 
     @Override
