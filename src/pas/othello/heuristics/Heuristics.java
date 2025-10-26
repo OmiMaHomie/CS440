b276b62f2d92d1d1f9fcdf7abe2108bb365baa21
Alex McBrier <alexmcbrier@icloud.com>
2025-10-26 00:49:38 -0400

    now in huersitic have the factors of piece count, frontier size, and flips with different weights doing much better now winning majority games random.

--- src/pas/othello/heuristics/Heuristics.java
+++ src/pas/othello/heuristics/Heuristics.java
@@ -28,13 +28,14 @@ public class Heuristics
         PlayerType mytype = node.getCurrentPlayerType();
         PlayerType opponentType = node.getOtherPlayerType();
         Game newGame = new Game(node.getGameView());
-        int numRows = newGame.getBoard().getNumRows();
+        int numRows = newGame. getBoard().getNumRows();
         int numCols = newGame.getBoard().getNumCols();
+        PlayerType[][] board = newGame.getView().getCells();
         //flips
         //this  uses the wouldSandwichOppositePlayerInDirection which estimates the strength of a move with its flips
 
         int flips = 0;
-        Set<Coordinate> legalMoves = newGame.getView().getFrontier(node.getCurrentPlayerType()); 
+        Set<Coordinate> legalMoves = newGame.getView().getFrontier(mytype); 
         for (Coordinate move : legalMoves) { //iterating through frontier of legal moves of the game state
 
             for (Direction d : Direction.values()) { //each direction of potential flips
@@ -43,6 +44,28 @@ public class Heuristics
                 }
             }
         }
-        return flips;
+        //num pieces on the board of me vs opponent
+        int myPieceCount = 0;
+        int opponentPieceCount = 0;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (board[i][j] == mytype) {
+                    myPieceCount += 1;
+                } 
+                else if (board[i][j] == opponentType) {
+                    opponentPieceCount += 1;
+                } 
+            }
+        }
+        int pieceDifference = myPieceCount - opponentPieceCount;
+
+
+        //num moves, just comparing the size of my frontier and opponents then translating to a var
+        int myNumMoves = legalMoves.size();
+        int opponentNumMoves = newGame.getView().getFrontier(opponentType).size(); 
+        int frontierDifference = myNumMoves - opponentNumMoves;
+
+        int overallHueristicScore = frontierDifference * 3 + flips * 2 + pieceDifference;
+        return overallHueristicScore;
     }
 }
