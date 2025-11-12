140a25989f9fd08243c0f535f7308ea5e40acdb8
Om Khadka <om.khadka.101@gmail.com>
2025-11-11 19:29:21 -0500

    v1 of zombayes, got my best interpretation of fields needed for this algo

--- src/labs/zombayes/models/NaiveBayes.java
+++ src/labs/zombayes/models/NaiveBayes.java
@@ -30,10 +30,22 @@ public class NaiveBayes
     // parameters of the model
     // TODO: decide what fields you need and what datatypes they should be and add them here!
 
+    private Map<Integer, Double> classPriors; // P(class)
+    private Map<Integer, List<Map<Integer, Double>>> discreteProbabilities; // P(feature_value | class) for the discrete features
+    private Map<Integer, List<Double>> continuousMeans; // the mean for continuous features per class
+    private Map<Integer, List<Double>> continuousStdDevs; // std dev for continuous features per class
+    private double smoothingAlpha = 1.0; // smooting val (laplace)
+
     public NaiveBayes(List<Pair<FeatureType, Integer> > featureHeader)
     {
         this.featureHeader = featureHeader;
+
         // TODO: if you add fields you will need to initialize them here!
+
+        this.classPriors = new HashMap<>();
+        this.discreteProbabilities = new HashMap<>();
+        this.continuousMeans = new HashMap<>();
+        this.continuousStdDevs = new HashMap<>();
     }
 
     public List<Pair<FeatureType, Integer> > getFeatureHeader() { return this.featureHeader; }
