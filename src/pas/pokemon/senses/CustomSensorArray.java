dadc4045b2951e3ff576de8c6e14b57a88ebe998
Om Khadka <om.khadka.101@gmail.com>
2025-11-24 14:16:05 -0500

    slightly fixed the sensor array. cleaned up the code & added a bit more datapoints to look at. In specfici the pokemon statuses

--- src/pas/pokemon/senses/CustomSensorArray.java
+++ src/pas/pokemon/senses/CustomSensorArray.java
@@ -1,11 +1,8 @@
 package src.pas.pokemon.senses;
 
-
+// SYSTEM IMPORTS
 import java.util.ArrayList;
 import java.util.List;
-
-// SYSTEM IMPORTS
-
 
 // JAVA PROJECT IMPORTS
 import edu.bu.pas.pokemon.agents.senses.SensorArray;
@@ -21,75 +18,103 @@ import edu.bu.pas.pokemon.core.enums.Type;
 public class CustomSensorArray
     extends SensorArray
 {
-
-    // TODO: make fields if you want!
+    //
+    // FIELDS
+    //
     int numFeatures;
 
+    //
+    // CONSTRUCTOR(S)
+    //
     public CustomSensorArray()
     {
-        // TODO: intialize those fields if you make any!
-        numFeatures = 21; //number of features
+        numFeatures = 29; // # of features
     }
+
+    //
+    // GET/SET
+    //
     public int getNumFeatures() {
         return numFeatures;
     }
-    public Matrix getSensorValues(final BattleView state, final MoveView action)
-    {
 
-
+    //
+    // METHODS
+    //
+    
         // TODO: Convert a BattleView and a MoveView into a row-vector containing measurements for every sense
         // you want your neural network to have. This method should be called if your model is a q-based model
         // currently returns 64 random numbers
-        List<Double> battleFeatures = new ArrayList<>();
-        //Getting action attributes
-        battleFeatures.add((double) action.getAccuracy());
-        battleFeatures.add((double) action.getPP());
-        battleFeatures.add((double) action.getPower());
-        battleFeatures.add((double) action.getCriticalHitRatio());
-        battleFeatures.add((double) action.getPriority());
-        battleFeatures.add((double) action.getNumDisabledTurnsRemaining());
-        if (action.getCategory() == Move.Category.PHYSICAL) {
-            battleFeatures.add((double) 1);
-        }
-        if (action.getCategory() == Move.Category.SPECIAL) {
-            battleFeatures.add((double) 2);
-        }
-        if (action.getCategory() == Move.Category.STATUS) {
-            battleFeatures.add((double) 3);
-        }
-        //Getting player 1 state values
-        battleFeatures.add((double) state.getTeam1View().getLastDamageTaken());
-        battleFeatures.add((double) state.getTeam1View().getNumLightScreenTurnsRemaining());
-        battleFeatures.add((double) state.getTeam1View().getNumReflectTurnsRemaining());
-        battleFeatures.add((double) state.getTeam1View().getActivePokemonView().getLastDamageDealt());
-        battleFeatures.add((double) state.getTeam1View().getActivePokemonView().getHeight().ordinal());
-        //type 1 team 1
-        Type type1team1 = state.getTeam1View().getActivePokemonView().getCurrentType1();
-        battleFeatures.add((double) type1team1.ordinal());
-        //type 2 team 1
-        Type type2team1 = state.getTeam1View().getActivePokemonView().getCurrentType2();
-        battleFeatures.add((double) type2team1.ordinal());
+    // Gets all vals that MIGHT be used for calculcating util. val, and put into an array.
+    public Matrix getSensorValues(final BattleView state, final MoveView action)
+    {
+        // We will collect 3 main types of info:
+        // General sensor values (of pokemons, the battlefield, etc.)
+        // Individual pokemon stats
+        // Move stats
+
+        List<Double> features = new ArrayList<>();
+
+        addPokemonState(features, state.getTeam1View().getActivePokemonView(), true); // Our pokemon
+        addPokemonState(features, state.getTeam2View().getActivePokemonView(), false); // Their pokemon
         
+        addMoveFeatures(features, action);
+        
+        addTeamFeatures(features, state.getTeam1View(), true); // Our team
+        addTeamFeatures(features, state.getTeam2View(), false); // Their team
 
-        //Getting player 2 state values
-        battleFeatures.add((double) state.getTeam2View().getLastDamageTaken());
-        battleFeatures.add((double) state.getTeam2View().getNumLightScreenTurnsRemaining());
-        battleFeatures.add((double) state.getTeam2View().getNumReflectTurnsRemaining());
-        battleFeatures.add((double) state.getTeam2View().getActivePokemonView().getLastDamageDealt()); 
-        battleFeatures.add((double) state.getTeam2View().getActivePokemonView().getHeight().ordinal());
-        //type 1 team 2
-        Type type1team2 = state.getTeam2View().getActivePokemonView().getCurrentType1();
-
-        battleFeatures.add((double) type1team2.ordinal());
-        //type 2 team 2
-        Type type2team2 = state.getTeam2View().getActivePokemonView().getCurrentType2();
-        battleFeatures.add((double) type2team2.ordinal());
-        Matrix rowVector =  Matrix.zeros(1, battleFeatures.size()); //making the row vector size of feature list
-        for (int i = 0; i < battleFeatures.size(); i++) {
-            rowVector.set(0, i, battleFeatures.get(i));
+        //making the row vector size of feature list
+        Matrix rowVector = Matrix.zeros(1, features.size());
+        for (int i = 0; i < features.size(); i++) {
+            rowVector.set(0, i, features.get(i));
         }
         return rowVector;
     }
 
+    private void addPokemonState(List<Double> features, PokemonView pokemon, boolean isOurs) {
+        // HP
+        double hpRatio = (double) pokemon.getCurrentStat(Stat.HP) / 
+                        pokemon.getInitialStat(Stat.HP);
+        features.add(hpRatio);
+        
+        // status conditions
+        NonVolatileStatus status = pokemon.getNonVolatileStatus();
+        features.add(status == NonVolatileStatus.SLEEP ? 1.0 : 0.0);
+        features.add(status == NonVolatileStatus.POISON ? 1.0 : 0.0);
+        features.add(status == NonVolatileStatus.BURN ? 1.0 : 0.0);
+        features.add(status == NonVolatileStatus.PARALYSIS ? 1.0 : 0.0);
+        features.add(status == NonVolatileStatus.FREEZE ? 1.0 : 0.0);
+        
+        // other statues (more unpreictable)
+        features.add(pokemon.getFlag(Flag.CONFUSED) ? 1.0 : 0.0);
+        features.add(pokemon.getFlag(Flag.TRAPPED) ? 1.0 : 0.0);
+        features.add(pokemon.getFlag(Flag.FLINCHED) ? 1.0 : 0.0);
+        
+        // lv
+        features.add((double) pokemon.getLevel() / 100.0);
+    }
 
+    private void addMoveFeatures(List<Double> features, MoveView action) {
+        // move prop.
+        features.add(action.getPower() != null ? (double) action.getPower() / 200.0 : 0.0);
+        features.add(action.getAccuracy() != null ? (double) action.getAccuracy() / 100.0 : 1.0);
+        features.add((double) action.getPP() / 40.0); // normalize PP
+        features.add((double) action.getPriority());
+        
+        // move category
+        features.add(action.getCategory() == Move.Category.PHYSICAL ? 1.0 : 0.0);
+        features.add(action.getCategory() == Move.Category.SPECIAL ? 1.0 : 0.0);
+        features.add(action.getCategory() == Move.Category.STATUS ? 1.0 : 0.0);
+    }
+
+    private void addTeamFeatures(List<Double> features, TeamView team, boolean isOurs) {
+        // # of alive pokemons
+        int aliveCount = 0;
+        for (int i = 0; i < team.size(); i++) {
+            if (!team.getPokemonView(i).hasFainted()) {
+                aliveCount++;
+            }
+        }
+        features.add((double) aliveCount / 6.0); // normalized
+    }
 }
